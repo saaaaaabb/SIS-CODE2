647de3cbc8205bd70e3d60a7671e0549583cdbd0
saaaaaabb <120376865+saaaaaabb@users.noreply.github.com>
2023-02-06 15:37:02 +0800

    Implement database using json

--- clone(no view record and delete).cpp
+++ clone(no view record and delete).cpp
@@ -4,7 +4,86 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "nlohmann/json.hpp"
+#include <map>
+using ordered_json = nlohmann::ordered_json;
 using namespace std;
+string firstname, lastname, course, address, student;
+int p_grade, c_grade, m_grade, e_grade, cs_grade;
+
+map<string, string>studentdata;
+
+map<string, map<string, string>>studentlist =
+{
+	{student, studentdata}
+
+};
+ordered_json studentdb;
+void viewAll() {
+	for (auto const& students : studentlist) {
+		if (students.first.empty()) continue;
+		cout << students.first << endl;
+		cout << "First Name: " << studentlist[students.first]["First Name"] << endl;
+		cout << "Last Name: " << studentlist[students.first]["Last Name"] << endl;
+		cout << "Course: " << studentlist[students.first]["Course"] << endl;
+		cout << "Address: " << studentlist[students.first]["Address"] << endl;
+		cout << "Physics: " << studentlist[students.first]["Physics"] << endl;
+		cout << "Chemistry: " << studentlist[students.first]["Chemistry"] << endl;
+		cout << "Math: " << studentlist[students.first]["Math"] << endl;
+		cout << "English: " << studentlist[students.first]["English"] << endl;
+		cout << "Computer Science: " << studentlist[students.first]["Computer Science"] << endl;
+		cout << endl;
+	}
+}
+void updateMap(string id) {
+	studentlist[id] =
+	{
+	{"First Name", firstname},
+	{"Last Name", lastname},
+	{"Course", course},
+	{"Address", address},
+	{"Physics",to_string(p_grade)},
+	{"Chemistry", to_string(c_grade)},
+	{"Math", to_string(m_grade)},
+	{"English", to_string(e_grade)},
+	{"Computer Science", to_string(cs_grade)}
+	};
+}
+void serialize() {
+	ofstream userout;
+	studentdb.clear();
+	for (auto const& id : studentlist) {
+		if (id.first.empty()) continue;
+		studentdb[id.first]["First Name"] = studentlist[id.first]["First Name"];
+		studentdb[id.first]["Last Name"] = studentlist[id.first]["Last Name"];
+		studentdb[id.first]["Course"] = studentlist[id.first]["Course"];
+		studentdb[id.first]["Address"] = studentlist[id.first]["Address"];
+		studentdb[id.first]["Physics"] = studentlist[id.first]["Physics"];
+		studentdb[id.first]["Chemistry"] = studentlist[id.first]["Chemistry"];
+		studentdb[id.first]["Math"] = studentlist[id.first]["Math"];
+		studentdb[id.first]["English"] = studentlist[id.first]["English"];
+		studentdb[id.first]["Computer Science"] = studentlist[id.first]["Computer Science"];
+	}
+	userout.open("studentdb.json", ios::out);
+	userout << std::setw(4) << studentdb << std::endl;
+	userout.close();
+}
+void deserialize() {
+	ifstream studentin("studentdb.json");
+	studentin >> studentdb;
+	for (auto& id : studentdb.items()) {
+		studentlist[id.key()]["First Name"] = studentdb[id.key()]["First Name"].get<string>();
+		studentlist[id.key()]["Last Name"] = studentdb[id.key()]["Last Name"].get<string>();
+		studentlist[id.key()]["Course"] = studentdb[id.key()]["Course"].get<string>();
+		studentlist[id.key()]["Address"] = studentdb[id.key()]["Address"].get<string>();
+		studentlist[id.key()]["Physics"] = studentdb[id.key()]["Physics"].get<string>();
+		studentlist[id.key()]["Chemistry"] = studentdb[id.key()]["Chemistry"].get<string>();
+		studentlist[id.key()]["Math"] = studentdb[id.key()]["Math"].get<string>();
+		studentlist[id.key()]["English"] = studentdb[id.key()]["English"].get<string>();
+		studentlist[id.key()]["Computer Science"] = studentdb[id.key()]["Computer Science"].get<string>();
+	}
+	studentin.close();
+}
 int login()
 {
 	string username;
@@ -43,72 +122,56 @@ void searchRecord() {
 	string id;
 	cout << "Enter Student ID: ";
 	cin >> id;
-	string filename =  id + ".txt";
-	fstream myFile;
-	myFile.open(filename, ios::in);
-	if (myFile.is_open()) {
-		string line;
-		while (getline(myFile, line)) {
-			cout << line << endl;
-		}
-		myFile.close();
+	if (studentlist.contains(id)) {
+		cout << "First Name: " << studentlist[id]["First Name"] << endl;
+		cout << "Last Name: " << studentlist[id]["Last Name"] << endl;
+		cout << "Course: " << studentlist[id]["Course"] << endl;
+		cout << "Address: " << studentlist[id]["Address"] << endl;
+		cout << "Physics: " << studentlist[id]["Physics"] << endl;
+		cout << "Chemistry: " << studentlist[id]["Chemistry"] << endl;
+		cout << "Math: " << studentlist[id]["Math"] << endl;
+		cout << "English: " << studentlist[id]["English"] << endl;
+		cout << "Computer Science: " << studentlist[id]["Computer Science"] << endl;
 	}
 }
 void removeStudent() {
 	string id;
 	cout << "Enter Student ID Number: ";
 	cin >> id;
-	string filename = "C:\\Users\\TONI\\source\\repos\\clone(no view record and delete)\\" + id + ".txt";
-	const char* filenamechar = filename.c_str();
-	if (remove(filenamechar) != 0)cout << "Failed" << endl;
-	else cout << "Deletion Success" << endl;
+	if (studentlist.erase(id) <= 0)cout << "Failed" << endl;
+	else { 
+		studentlist.erase(id);
+		cout << "Deletion Success" << endl; 
+	}
 }
 void editStudent() {
-	string firstname, lastname, course, address, student;
-	int p_grade, c_grade, m_grade, e_grade, cs_grade;
 
 	cout << "Enter Student ID Number: ";
 	getline(cin, student);
-	cout << "New First Name: ";
+	cout << "First Name: ";
 	getline(cin, firstname);
-	cout << "New Last Name: ";
+	cout << "Last Name: ";
 	getline(cin, lastname);
-	cout << "New Course: ";
+	cout << "Course: ";
 	getline(cin, course);
-	cout << "New Address: ";
+	cout << "Address: ";
 	getline(cin, address);
-	cout << "Enter New marks in Physics out of 100 : ";
+	cout << "Enter marks in Physics out of 100 : ";
 	cin >> p_grade;
-	cout << "Enter New marks in Chemistry out of 100 : ";
+	cout << "Enter marks in Chemistry out of 100 : ";
 	cin >> c_grade;
-	cout << "Enter New marks in Maths out of 100 : ";
+	cout << "Enter marks in Maths out of 100 : ";
 	cin >> m_grade;
-	cout << "Enter New marks in English out of 100 : ";
+	cout << "Enter marks in English out of 100 : ";
 	cin >> e_grade;
-	cout << "Enter New marks in Computer science out of 100 : ";
+	cout << "Enter marks in Computer science out of 100 : ";
 	cin >> cs_grade;
-	cout << "Your New Average Grade is: " << calculate(p_grade, c_grade, m_grade, e_grade, cs_grade) << endl;
+	cout << "Your Average Grade is: " << calculate(p_grade, c_grade, m_grade, e_grade, cs_grade) << endl;
 
-	string filename = student + ".txt";
-	ofstream myFile2;
-	myFile2.open(filename, ios::out);
-	if (myFile2.is_open()) {
-
-		myFile2 << "New Student ID Number: " << student << endl;
-		myFile2 << "New First Name: " << firstname << endl;
-		myFile2 << "New Last Name: " << lastname << endl;
-		myFile2 << "New Course: " << course << endl;
-		myFile2 << "New Address: " << address << endl;
-		myFile2 << "New Marks in Physics: " << p_grade << endl;
-		myFile2 << "New Marks in Chemistry: " << c_grade << endl;
-		myFile2 << "New Marks in Maths: " << m_grade << endl;
-		myFile2 << "New Marks in English: " << e_grade << endl;
-		myFile2 << "New Marks in Computer Science: " << cs_grade << endl;
-		myFile2 << "Your New Average is: " << calculate(p_grade, c_grade, m_grade, e_grade, cs_grade) << endl;
-		myFile2.close();
-	}
+	updateMap(student);
 }
 int main() {
+	deserialize();
 	login();
 
 	while (true) {
@@ -117,7 +180,8 @@ int main() {
 		cout << "1.ADD/EDIT STUDENT RECORD" << endl;
 		cout << "2.SEARCH STUDENT" << endl;
 		cout << "3.REMOVE STUDENT" << endl;
-		cout << "4.EXIT" << endl;
+		cout << "4.VIEW ALL" << endl;
+		cout << "5.EXIT" << endl;
 		int userinput;
 		cin >> userinput;
 		switch (userinput) {
@@ -125,6 +189,7 @@ int main() {
 			system("cls");
 			cin.ignore();
 			editStudent();
+			serialize();
 			continue;
 		case 2:
 			system("cls");
@@ -133,8 +198,13 @@ int main() {
 		case 3:
 			system("cls");
 			removeStudent();
+			serialize();
 			continue;
 		case 4:
+			system("cls");
+			viewAll();
+			continue;
+		case 5:
 			system("cls");
 			cout << "THANK YOU" << endl;
 			break;
